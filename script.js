29b402ec37f0ca74332a189af8fdaa64c1906456
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-07-17 20:39:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-
-const getSum = () => {
+let priceValue=document.querySelectorAll(".price");
+let sum=0;
+let tables=document.querySelector("table");
+let newRow=document.createElement("tr");
+let newCol=document.createElement("td");
+const getSum = (event) => {
 //Add your code here
+	event.preventDefault();
+	
+	for(let t of priceValue){
+		sum+=parseInt(t.innerText);
+		//console.log(sum);
+	}
+	newCol.innerText=sum;
+	newRow.append(newCol);
+	tables.append(newRow);
+	
+	
+
   
 };
 
